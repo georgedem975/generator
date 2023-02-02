721ebf423b4e6073bd87d30d87b31851ea18be83
georgedem <georgiidemyanov@yandex.ru>
2023-02-02 04:59:52 +0300

    feat: GenerateLevelsImageHandler

--- algorithmsLibrary/FirstAlgorithmsDll/GeneratorHandler.h
+++ algorithmsLibrary/FirstAlgorithmsDll/GeneratorHandler.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "PixelRGB.h"
+
+class GeneratorHandler
+{
+public:
+    virtual PixelRGB GetPixelByLevel(int level) = 0;
+    virtual void SetNext(GeneratorHandler* nextHandler) = 0;
+};
